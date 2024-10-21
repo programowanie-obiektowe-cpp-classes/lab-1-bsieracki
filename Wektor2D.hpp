89ed0186b6c0c156441b34f951363e7364146554
Bartosz Sieracki <105609470+bsieracki@users.noreply.github.com>
2024-10-21 20:34:47 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,46 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+private:
+    double x;
+    double y;
+
+public:
+    Wektor2D() : x(0), y(0)
+    {
+      std::cout << "[" << x << ", " << y << "]\n";
+    }
+
+    Wektor2D(double xValue, double yValue) : x(xValue), y(yValue)
+    {
+      std::cout << "[" << x << ", " << y << "]\n";
+    }
+
+    void setX(double xValue)
+    {
+        x = xValue;
+    }
+
+    void setY(double yValue)
+    {
+        y = yValue;
+    }
+
+    double getX()const
+    {
+        return x;
+    }
+
+    double getY()const
+    {
+        return y;
+    }
+
+    friend Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2) {
+        return Wektor2D(v1.x + v2.x, v1.y + v2.y);
+    }
+    
+    friend double operator*(const Wektor2D& v1, const Wektor2D& v2) {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
 };
+
